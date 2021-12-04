eb1cfc7ad325327ea85f60041506f0e4d9f39e15
ganyuke <personalemailforedits@gmail.com>
2021-12-03 18:59:03 -0800

    Fix A notes in Tender Strength.

--- tenderStrength.h
+++ tenderStrength.h
@@ -11,14 +11,14 @@ const int ts_totalNotes = 64;
 
 const int ts_melody[] PROGMEM = {
   FS5, D5, B4, FS5, E5, 0,
-  E5, CS5, A4, E5, D5, 0,
-  B4, CS5, D5, A5, G5, G5, FS5, E5, D5, 0,
+  E5, CS5, NOTE_A4, E5, D5, 0,
+  B4, CS5, D5, NOTE_A5, G5, G5, FS5, E5, D5, 0,
   FS5, D5, B4, FS5, E5, 0,
-  E5, CS5, A4, E5, D5, 0,
-  B4, CS5, D5, A5, G5, FS5, E5, D5, 0,
+  E5, CS5, NOTE_A4, E5, D5, 0,
+  B4, CS5, D5, NOTE_A5, G5, FS5, E5, D5, 0,
   FS6, D6, B5, FS6, E6, 0,
-  E6, CS6, A5, E6, D6, 0,
-  B5, CS6, D6, A6, G6, B5, CS6, CS6, D6
+  E6, CS6, NOTE_A5, E6, D6, 0,
+  B5, CS6, D6, NOTE_A6, G6, B5, CS6, CS6, D6
 };
 
 const double ts_noteDurations[] PROGMEM = {
